4cf4431d71f4b8a8c76b66879500fa8e26269442
Vishnu Mishra <vishnu.mishra@tecsolsoftware.com>
2015-08-15 19:57:48 +0530

    get user data from phase is completed

--- api/controllers/PhaseController.js
+++ api/controllers/PhaseController.js
@@ -4,18 +4,57 @@
  * @description :: Server-side logic for managing phases
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-var getLocationId = function(phaseId){
-		var locations =[]; 
-		Location.findAll({"where":{"phase_id":params}}).then(function(result){
-			result = (result == null)?[]:result;
-			result.forEach(function(locationId){
-				locations.push(locationId)
-			})
-			return locations;
-		}).catch(function(err){
-			return [];
+var getLocationsFromPhaseId = function(phaseId,cb){
+	var locations =[]; 
+	Location.findAll({where:{phase_id:phaseId}}).then(function(result){
+		result = (result == null)?[]:result;
+		result.forEach(function(location){
+			locations.push(location.id)
 		})
+		cb(null,locations)
+		return ;
+	}).catch(function(err){
+		cd(err,locations)
+		return;
+	})
 }
+
+var getPhaseIdFromName = function(phase,cb){
+	var phaseId;
+	Phase.findOne({"where":{"name":phase}}).then(function(phaseData){
+		phaseId = phaseData.id
+		cb(null,phaseId);
+	}).catch(function(err){
+		cb(err,phaseId);		
+	})
+};
+
+var getUserIdFromLocationId = function(locations,cb){
+var users =[]; 
+	UserLocation.findAll({"where":{locationId:locations}}).then(function(result){
+		result = (result == null)?[]:result;
+		result.forEach(function(data){
+			users.push(data.userId)
+		})
+		cb(null,users)
+		return ;
+	}).catch(function(err){
+		cd(err,users)
+		return;
+	})	
+}
+var getUserDetailsFromUserId = function(userId,cb){
+	User.findAll({where:{id:userId},attributes:['id','firstName','lastName']}).then(function(result){
+		result = (result == null)?[]:result;
+		cb(null,result)
+		return ;
+	}).catch(function(err){
+		cd(err,[])
+		return;
+	})
+}
+
+
 module.exports = {
 	create:function(req,res,next){
 		try{
@@ -72,43 +111,43 @@ module.exports = {
 		try{
 			var phase = req.body.phase;
 			var phaseId;
-			Phase.findOne({"where":{"name":phase}}).then(function(phaseData){
-				if(phaseData){
-					phaseId = phaseData.id;
-				}else{
-					res.json({sucess:false,user:"phase name not exist"})
-					return;
-				}
-			})
-			if(phaseId){
-				var userId = [];
-				var locations = getLocationId(phaseId);
-				UserLocation.findAll({"where":{locationId:locations}}).then(function(users){
-					users.forEach(function(user){
-						userId.push(user.id);
-					})
-					if(userId>0){
-						User.findAll({"where":{"id":userId}}).then(function(userData){
-							res.json({sucess:true,userData:userData})
-							return;
-						}).catch(function(err){
-							res.json({sucess:false,err:err});
-							return;				
-						})
-					}else{
-						res.json({sucess:true,userData:[]})
-						return;
-					}
-				}).catch(function(err){
-					res.json({sucess:false,err:err});
-					return;				
-				})
-			}else{
-				res.json({sucess:false,err:err});
-				return;
-			}
+			async.waterfall([
+	            function(cb) {
+	                getPhaseIdFromName(phase, cb);
+	            },
+	            function(phaseId, cb) {
+	            	if(!phaseId){
+	            		res.json({sucess:true,data:[]})
+	            		return;
+	            	}
+	            	getLocationsFromPhaseId(phaseId,cb)
+	            },
+	            function(locations, cb) {
+	            	if((!locations) || locations.length == 0){
+	            		res.json({sucess:true,data:[]})
+	            		return;
+	            	}
+	            	getUserIdFromLocationId(locations,cb)
+	            },
+	            function(users, cb) {
+	            	if((!users) || users.length == 0){
+	            		res.json({sucess:true,data:[]})
+	            		return;
+	            	}
+	            	getUserDetailsFromUserId(users,cb)
+	            }
+	        ],
+            function(err, results) {
+                res.json({
+                    success:true,
+                    userData:results
+                })
+            })
 		}catch(err){
-			res.json({sucess:false,err:err});			
+	        res.json({
+	            success:true,
+	            userData:results
+	        })
 		}
 	}
 };
